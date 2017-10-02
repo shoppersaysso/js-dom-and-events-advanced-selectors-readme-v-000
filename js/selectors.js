b9505abbc55b094dbc2dde6a985f89b3cd6fb912
Sara Hopper <sara.hopper@gmail.com>
2017-10-02 01:51:55 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
 
 
+
   function paragraphSelector(){
-    
+    $('#p')
   };
 
   function lastImageSelector(){
-    
+
   };
 
   function ninjaBabySelector(){
-    
+
   };
 
   function divSelector(){
-    
+
   };
 
   function firstListItem(){
-    
+
   };
 
 });
